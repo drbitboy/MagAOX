17a382183b7ee320c26c77008f6750bff717e18a
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-10-16 18:19:33 -0400

    Update comments in indiserver.c

--- INDI/INDI/indiserver.c
+++ INDI/INDI/indiserver.c
@@ -13,25 +13,28 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301  USA
 
- * argv lists names of Driver programs to run or sockets to connect for Devices.
- * Drivers are restarted if they exit or connection closes.
- * Each local Driver's stdin/out are assumed to provide INDI traffic and are
- *   connected here via pipes. Local Drivers' stderr are connected to our
- *   stderr with date stamp and driver name prepended.
- * We only support Drivers that advertise support for one Device. The problem
- *   with multiple Devices in one Driver is without a way to know what they
- *   _all_ are there is no way to avoid sending all messages to all Drivers.
+ * argv lists prefix paths of FIFO files (named pipes) of Driver programs
+     to run or sockets to connect for Devices.
+ * Drivers are assumed to be restarted elsewhere (resurrector)
+ * Each local Driver arg /opt/MagAOX/drivers/fifos/device.{in|out}
+     is assumed to provide INDI traffic via named pipes. Local Drivers'
+     stderrs are unused
+ * We only support Drivers that advertise support for one Device. The
+     problem with multiple Devices in one Driver is without a way to
+     know what they _all_ are there is no way to avoid sending all
+     messages to all Drivers.
  * Outbound messages are limited to Devices and Properties seen inbound.
- *   Messages to Devices on sockets always include Device so the chained
- *   indiserver will only pass back info from that Device.
- * All newXXX() received from one Client are echoed to all other Clients who
- *   have shown an interest in the same Device and property.
- *
+ * Messages to Devices on sockets always include Device so the chained
+     indiserver will only pass back info from that Device.
+ * All newXXX() received from one Client are echoed to all other Clients
+     which have shown an interest in the same Device and property.
+  
  * 2017-01-29 JM: Added option to drop stream blobs if client blob queue is
- * higher than maxstreamsiz bytes
- *
+                  higher than maxstreamsiz bytes
+  
  * Implementation notes:
  *
  * We open each driver and open a server socket listening for INDI clients.
